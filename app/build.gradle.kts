afcbef383368d548e30899902fa402c4bca666dc
coderylicode <rylidavis94@gmail.com>
2022-03-21 23:30:30 -0700

    Adding Apollo Plugins and Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id ("com.android.application")
     id ("org.jetbrains.kotlin.android")
+    id("com.apollographql.apollo3").version("3.1.0")
+
 }
 
 android {
@@ -51,8 +53,12 @@ android {
         implementation("androidx.constraintlayout:constraintlayout:2.1.3")
         implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
         implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
+        implementation("com.apollographql.apollo3:apollo-runtime:3.1.0")
         testImplementation("junit:junit:4.13.2")
         androidTestImplementation("androidx.test.ext:junit:1.1.3")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     }
+}
+apollo {
+    packageName.set("com.example.aniMod")
 }
